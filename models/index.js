fdeef61f3df2d32da80e9bf8056bcf2a06f9354c
baolequoc <bao.lq@holistics.io>
2022-09-24 22:57:58 +0700

    init project

---
+++ models/index.js
@@ -0,0 +1,16 @@
+const mapGroup = new Map();
+mapGroup.set(19110327, 'Le Quoc Bao');
+mapGroup.set(19110323, 'Ta Quoc Anh');
+mapGroup.set(19110373, 'Pham Quang Hung');
+
+const myGroup = [
+  {
+    id: 19110327,
+    name: 'Le Quoc Bao',
+  },
+];
+
+module.exports = {
+  mapGroup,
+  myGroup,
+};

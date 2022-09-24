50cf0c9327d428efb0ffe708c2c51e126c0a34e0
baolequoc <bao.lq@holistics.io>
2022-09-24 23:12:13 +0700

    re-console URL

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 // allow cors
 app.use((req, res, next) => {
-  console.log(`URL: ${req.url}`);
+  console.log(`URL: ${req.method}  ${req.url}`);
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers',
